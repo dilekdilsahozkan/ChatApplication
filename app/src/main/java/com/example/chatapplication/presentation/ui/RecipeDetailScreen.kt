5061e78ea427f847a75854e9e98f4eafaa78e31e
Mert Uludag <mertuludag2316@gmail.com>
2024-05-20 01:54:42 +0300

    Not last version

--- app/src/main/java/com/example/chatapplication/presentation/ui/RecipeDetailScreen.kt
+++ app/src/main/java/com/example/chatapplication/presentation/ui/RecipeDetailScreen.kt
@@ -1,9 +1,7 @@
 package com.example.chatapplication.presentation.ui
 
 import androidx.compose.runtime.Composable
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -14,8 +12,6 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,12 +23,9 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat
-import com.chatapplication.presentation.ui.theme.*
 import com.example.chatapplication.R
 import com.example.chatapplication.ui.theme.DarkGray
 import com.example.chatapplication.ui.theme.Gray
